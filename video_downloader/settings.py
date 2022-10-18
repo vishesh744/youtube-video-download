6fe03d62f13f50ffc319b0c9c96cf4380e7910f8
vishesh744 <89727337+vishesh744@users.noreply.github.com>
2022-10-18 11:54:48 +0530

    seet

--- video_downloader/settings.py
+++ video_downloader/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-e)u2@#kn&qij@+ujtrojam2_4m8h6b0bb#g8-6#$c87v6n60ix
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','youtube-video-downloadd.herokuapp.com']
+ALLOWED_HOSTS = ['youtube-video-downloadd.herokuapp.com']
 
 
 # Application definition
